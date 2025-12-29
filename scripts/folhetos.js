c054fcbe0cade41ae17463b81fe7ab2d62a18323
PaulaoTI <sistemassiscobra@gmail.com>
2025-12-29 10:23:51 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -1,23 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     const folhetos = [
-        { data: "07-Dez-25", arquivo: "7-dez-25.pdf" },
-        { data: "14-Dez-25", arquivo: "14-dez-25.pdf" },
-        { data: "21-Dez-25", arquivo: "21-dez-25.pdf" },
-        { data: "24-Dez-25", arquivo: "24-dez-25.pdf" },
-        { data: "25-Dez-25", arquivo: "25-dez-25.pdf" },
-        { data: "28-Dez-25", arquivo: "28-dez-25.pdf" },
+
+        { data: "01-Jan-26", arquivo: "01-jan-26.pdf" },
+        { data: "04-Jan-26", arquivo: "04-jan-26.pdf" },
+        { data: "11-Jan-26", arquivo: "11-jan-26.pdf" },
+        { data: "18-Jan-26", arquivo: "18-jan-26.pdf" },
+        { data: "25-Jan-26", arquivo: "25-jan-26.pdf" }
+
     ];
+
+    const coresFolhetos = [
+
+        "#ffcf31ff",
+        "#ffcf31ff",
+        "#ffcf31ff",
+        "#1cf715ff",
+        "#1cf715ff"
+
+    ];
+
+    const coresButtons = [
+
+        "#ffcf31ff",
+        "#ffcf31ff",
+        "#ffcf31ff",
+        "#1cf715ff",
+        "#1cf715ff"
+        
+    ]
 
     const container = document.getElementById("folhetosContainer");
 
-    folhetos.forEach(folheto => {
+    folhetos.forEach((folheto, index) => {
         const div = document.createElement("div");
         div.classList.add("div-folhetos");
 
+        const corDoTexto = coresFolhetos[index % coresFolhetos.length];
+
+        const corDoButton = coresButtons[index % coresFolhetos.length];
+
         div.innerHTML = `
-            <h1 class="text-folhetos">${folheto.data}</h1>
+            <h1 class="text-folhetos" style="color: ${corDoTexto};">${folheto.data}</h1>
             <a href="folhetos/${folheto.arquivo}" target="_blank">
-                <button class="button-folhetos">Baixar</button>
+                <button class="button-folhetos" style="background-color: ${corDoButton};">Baixar</button>
             </a>
         `;
 
