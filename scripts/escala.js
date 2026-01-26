d926856954a70fb4a85d136771ee1b2718adfea2
PaulaoTI <sistemassiscobra@gmail.com>
2026-01-26 16:06:48 -0300

    Update escala.js

--- scripts/escala.js
+++ scripts/escala.js
@@ -1,4 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const escalas = [
+        { nome: "Fevereiro", arquivo: "" }
+    ];
 
+    const container = document.getElementById("escalasContainer");
+
+    escalas.forEach(escala => {
+        const div = document.createElement("div");
+        div.classList.add("div-escala");
+
+        const disabled = escala.arquivo === "" ? "disabled" : "";
+        const link = escala.arquivo || "#";
+
+        div.innerHTML = `
+            <h1 class="text-escala">${escala.nome}</h1>
+            <a href="${link}">
+                <button class="button-folhetos" ${disabled}>Baixar</button>
+            </a>
+        `;
+
+        container.appendChild(div);
+    });
+});
+
+/*
 document.addEventListener("DOMContentLoaded", () => {
     const escalas = [
         { nome: "Janeiro", arquivo: "escalas/EscaladeJaneiro.jpg" }
@@ -22,29 +47,4 @@ document.addEventListener("DOMContentLoaded", () => {
         container.appendChild(div);
     });
 });
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     const escalas = [
-//         { nome: "Janeiro", arquivo: "" }
-//     ];
-
-//     const container = document.getElementById("escalasContainer");
-
-//     escalas.forEach(escala => {
-//         const div = document.createElement("div");
-//         div.classList.add("div-escala");
-
-//         const disabled = escala.arquivo === "" ? "disabled" : "";
-//         const link = escala.arquivo || "#";
-
-//         div.innerHTML = `
-//             <h1 class="text-escala">${escala.nome}</h1>
-//             <a href="${link}">
-//                 <button class="button-folhetos" ${disabled}>Baixar</button>
-//             </a>
-//         `;
-
-//         container.appendChild(div);
-//     });
-// });
+*/
