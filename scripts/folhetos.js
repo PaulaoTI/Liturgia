58f773581b9b60f3b568d71e426c5b24d4a79dcc
PaulaoTI <sistemassiscobra@gmail.com>
2026-05-25 10:51:42 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -2,28 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const folhetos = [
 
-        { data: "17-Mai-26", arquivo: "17-mai-26.pdf" },
-        { data: "23-Mai-26", arquivo: "23-mai-26.pdf" },
-        { data: "24-Mai-26", arquivo: "24-mai-26.pdf" },
         { data: "31-Mai-26", arquivo: "31-mai-26.pdf" },
-
+        { data: "04-Jun-26", arquivo: "4-jun-26.pdf" },
+        { data: "07-Jun-26", arquivo: "7-jun-26.pdf" },
+        { data: "14-Jun-26", arquivo: "14-jun-26.pdf" },
+        { data: "21-Jun-26", arquivo: "21-jun-26.pdf" },
+        { data: "28-Jun-26", arquivo: "28-jun-26.pdf" },
     ];
 
     const coresFolhetos = [
 
         "rgb(255, 213, 25)",
-        "rgb(255, 25, 25)",
-        "rgb(255, 25, 25)",
         "rgb(255, 213, 25)",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "rgb(255, 25, 25)",
 
     ];
 
     const coresButtons = [
 
         "rgb(255, 213, 25)",
-        "rgb(255, 25, 25)",
-        "rgb(255, 25, 25)",
         "rgb(255, 213, 25)",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "rgb(255, 25, 25)",
 
     ]
 
