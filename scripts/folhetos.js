7057cf0e10b0cfc67391df6ac7a3110add71c3a3
PaulaoTI <sistemassiscobra@gmail.com>
2026-07-28 11:45:36 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -1,32 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const folhetos = [
-    
-            { data: "28-Jun-26", arquivo: "28-jun-26.pdf" },
-            { data: "05-Jul-26", arquivo: "5-jul-26.pdf" },
-            { data: "12-Jul-26", arquivo: "12-jul-26.pdf" },
-            { data: "19-Jul-26", arquivo: "19-jul-26.pdf" },
-            { data: "26-Jul-26", arquivo: "26-jul-26.pdf" },
-        ];
-    
-        const coresFolhetos = [
-    
-            "rgb(255, 25, 25)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-    
-        ];
-    
-        const coresButtons = [
-    
-           "rgb(255, 25, 25)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-            "rgb(89, 206, 10)",
-        ];
+
+        { data: "28-Jun-26", arquivo: "02-08-26.pdf" },
+        { data: "05-Jul-26", arquivo: "09-08-26.pdf" },
+        { data: "12-Jul-26", arquivo: "16-08-26.pdf" },
+        { data: "19-Jul-26", arquivo: "25-08-26.pdf" },
+        { data: "26-Jul-26", arquivo: "30-08-26.pdf" },
+    ];
+
+    const coresFolhetos = [
+
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "#FFD700",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+    ];
+
+    const coresButtons = [
+
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+        "#FFD700",
+        "rgb(89, 206, 10)",
+        "rgb(89, 206, 10)",
+    ];
 
     const container = document.getElementById("folhetosContainer");
 
