4919fb00a1bd4752b099f5a8127c19383674f8df
PaulaoTI <sistemassiscobra@gmail.com>
2026-08-30 21:28:27 -0300

    Update folhetos.js

--- scripts/folhetos.js
+++ scripts/folhetos.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const folhetos = [
 
-        { data: "30-Ago-26", arquivo: "30-08-26.pdf" },
         { data: "06-Set-26", arquivo: "6-09-26.pdf" },
         { data: "13-Set-26", arquivo: "13-09-26.pdf" },
         { data: "20-Set-26", arquivo: "20-09-26.pdf" },
